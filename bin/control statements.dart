360afcd124faf4109cb497ced26933a55db102fd
sreedeviluminar <sreedevi@luminartechnolab.com>
2023-02-09 13:26:17 +0530

    initial commit

--- bin/control statements.dart
+++ bin/control statements.dart
@@ -17,30 +17,52 @@ void main() {
   int otp = 1234;
 
   /// nested if
-  if(username =="Admin"  &&  password == 'abc123'){
+  if (username == "Admin" && password == 'abc123') {
     print('email and password are correct');
-    if(otp == 123){
+    if (otp == 123) {
       print("Login Successful , Account created");
-    }else{
+    } else {
       print("otp verification failed");
     }
-  }else{
+  } else {
     print('email and password verification failed');
   }
 
   ///if else-if  ladder
-  var size = 'XXL';
-  if(size == 'xs'){
+  var size = 34;
+
+  if (size == 'xs') {
     print('your shirt size is xs');
-  }else if(size == 's'){
+  } else if (size == 's') {
     print('your shirt size is S');
-  }else if(size == 'M'){
+  } else if (size == 'M') {
     print('your shirt size is M');
-  }else if(size == 'L'){
+  } else if (size == 'L') {
     print('your shirt size is L');
-  }else if(size == 'XL'){
+  } else if (size == 'XL') {
     print('your shirt size is xl');
-  }else{
+  } else {
     print('Size not available');
+  }
+
+  ///Switch case
+  switch (size) {
+    case 28:
+      print('your shirt size is xs');
+      break;
+    case 30:
+      print('your shirt size is s');
+      break;
+    case 32:
+      print('your shirt size is m');
+      break;
+    case 34:
+      print('your shirt size is l');
+      break;
+    case 36:
+      print('your shirt size is xs');
+      break;
+    case 40:
+      print('your shirt size is s');
   }
 }
